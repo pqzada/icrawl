9d778b46611c7951d04ea5d4d551f162d50ad4d3
Pablo Quezada <pquezadar@neonline.cl>
2016-09-22 00:36:18 -0300

    Se añade robotx y meta para que sitio no sea indexado

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $actualizacion = $actualizacionFacade->get();
 	<title>InstaCrawl</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex">
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
